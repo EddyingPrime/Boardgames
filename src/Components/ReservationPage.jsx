f4230166e9d32976aa8a99bc018d49491674e854
lourainep <polouraine@gmail.com>
2023-11-22 21:33:59 +0800

    Update ReservationPage.jsx

--- src/Components/ReservationPage.jsx
+++ src/Components/ReservationPage.jsx
@@ -1,79 +1,111 @@
 import { useState } from "react";
 
-const ReservationPage = ({ option }) => {
-  const [name, setName] = useState("");
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("");
 
-  const handleReservationSubmit = (e) => {
+const cafes = ['Cafe A', 'Cafe B', 'Cafe C']; // Replace with your cafe list
+
+const ReservationPage = () => {
+  const [selectedDate, setSelectedDate] = useState('');
+  const [selectedTime, setSelectedTime] = useState('');
+  const [selectedSeat, setSelectedSeat] = useState('');
+  const [selectedCafe, setSelectedCafe] = useState('');
+  const [guestName, setGuestName] = useState('');
+
+  const handleDateChange = (e) => {
+    setSelectedDate(e.target.value);
+  };
+
+  const handleTimeChange = (e) => {
+    setSelectedTime(e.target.value);
+  };
+
+  const handleSeatChange = (e) => {
+    setSelectedSeat(e.target.value);
+  };
+
+  const handleCafeChange = (e) => {
+    setSelectedCafe(e.target.value);
+  };
+
+  const handleGuestNameChange = (e) => {
+    setGuestName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    // Add logic to handle the reservation submission
-    console.log("Reservation submitted:", { name, date, time });
-    // Add further logic like sending the reservation to a backend or displaying a confirmation message
+    console.log('Reservation details:', {
+      date: selectedDate,
+      time: selectedTime,
+      seat: selectedSeat,
+      cafe: selectedCafe,
+      guestName: guestName,
+    });
   };
 
   return (
-    <div>
-      <h2 className="text-2xl font-bold mb-4">{option} Reservation Page</h2>
-      <form onSubmit={handleReservationSubmit} className="mt-4">
-        {/* Form fields */}
+    <div className="max-w-md mx-auto mt-8 p-3 bg-white shadow-md rounded-md mb-5">
+      <h2 className="text-xl font-semibold mb-4">Make a Reservation</h2>
+      <form onSubmit={handleSubmit}>
         <div className="mb-4">
-          <label
-            htmlFor="name"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Your Name:
+          <label htmlFor="cafe" className="block text-sm font-semibold mb-2">
+            Select Cafe:
           </label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label
-            htmlFor="date"
-            className="block text-sm font-medium text-gray-700"
+          <select
+            id="cafe"
+            value={selectedCafe}
+            onChange={handleCafeChange}
+            className="w-full p-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
           >
-            Reservation Date:
+            <option value="">Select a cafe</option>
+            {cafes.map((cafe, index) => (
+              <option key={index} value={cafe}>
+                {cafe}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label htmlFor="date" className="block text-sm font-semibold mb-2">
+            Select Date:
           </label>
           <input
             type="date"
             id="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange"
-            required
+            value={selectedDate}
+            onChange={handleDateChange}
+            className="w-full p-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
           />
         </div>
-
         <div className="mb-4">
-          <label
-            htmlFor="time"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Reservation Time:
+          <label htmlFor="time" className="block text-sm font-semibold mb-2">
+            Select Time:
           </label>
           <input
             type="time"
             id="time"
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange"
-            required
+            value={selectedTime}
+            onChange={handleTimeChange}
+            className="w-full p-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
           />
         </div>
-
-        {/* Submit button */}
+        
+        <div className="mb-4">
+          <label htmlFor="guestName" className="block text-sm font-semibold mb-2">
+            Guest Name:
+          </label>
+          <input
+            type="text"
+            id="guestName"
+            value={guestName}
+            onChange={handleGuestNameChange}
+            placeholder="Enter guest name"
+            className="w-full p-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          />
+        </div>
         <button
           type="submit"
-          className="bg-orange text-white py-2 px-4 rounded-md hover:bg-orange-dark focus:outline-none focus:shadow-outline-orange active:bg-orange-darker"
+          className="bg-orange text-black py-2 px-4 rounded-md hover:bg-blue-600 transition-colors"
         >
-          Submit Reservation
+          Reserve Now
         </button>
       </form>
     </div>
