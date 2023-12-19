d431869f1f3500f8b1adbc9ad96acc42e18b2ded
lourainep <136150473+lourainep@users.noreply.github.com>
2023-12-20 05:44:31 +0800

    updated

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -16,20 +16,20 @@ const Hero = () => {
   }, [images.length]);
 
   return (
-    <div className="relative h-screen -mb-[5%]">
+    <div className="relative h-[60vh] overflow-hidden">
       {images.map((image, index) => (
         <img
           key={index}
           src={image}
           alt={`hero-${index}`}
-          className={`absolute w-full h-[70%] transition-opacity   duration-2000 ${
+          className={`absolute w-full h-full transition-opacity duration-2000 ${
             index === currentImage ? "opacity-80" : "opacity-0"
           }`}
         />
       ))}
       <div className="absolute inset-0 flex justify-center items-center">
-        <div className="bg-gray bg-opacity-70 mb-[20%] rounded-md p-2 max-w-md text-center">
-          <h1 className="text-5xl font-bold mb-4 text-orange">Welcome</h1>
+        <div className="bg-gray bg-opacity-70 rounded-md p-2 max-w-md text-center">
+        <h1 className="text-5xl font-bold mb-4 text-orange">Welcome</h1>
           <p className="mb-8 text-2xl">
             🎲 Are you ready to roll the dice and experience the joy of board
             games? Look no further!{" "}
@@ -43,3 +43,16 @@ const Hero = () => {
 };
 
 export default Hero;
+
+
+
+
+// <div className="bg-gray bg-opacity-70 mb-[20%] rounded-md p-2 max-w-md text-center">
+//           <h1 className="text-5xl font-bold mb-4 text-orange">Welcome</h1>
+//           <p className="mb-8 text-2xl">
+//             🎲 Are you ready to roll the dice and experience the joy of board
+//             games? Look no further!{" "}
+//             <span className="font-bold">Touch Grass</span> is your ultimate
+//             destination for all things board games and socialization.
+//           </p>
+//         </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,8 +8,50 @@ import TableTaft from '../assets/TableTaft.png';
 import GameOver from '../assets/GameOver.png';
 import Hungry from '../assets/Hungry.png';
 import Happy from '../assets/Happy.png';
+import List1 from '../assets/list1.jpg'
+
 
 const Home = () => {
+  const recommendedCafes = [
+    {
+      id: 1,
+      name: 'TableTaft Boardgame Cafe',
+      link: '/TableTaft',
+      image: TableTaft,
+    },
+    {
+      id: 2,
+      name: 'Game Over PH: Respawn',
+      link: '/GameOver',
+      image: GameOver,
+    },
+    {
+      id: 3,
+      name: 'Hungry Meeples',
+      link: '/Hungry',
+      image: Hungry,
+    },
+    {
+      id: 4,
+      name: 'Happy Beans',
+      link: '/Happy',
+      image: Happy,
+    },
+  ];
+
+
+  const videoLinks = [
+    "https://www.youtube.com/embed/video1",
+    "https://www.youtube.com/embed/video2",
+    "https://www.youtube.com/embed/video3",
+    "https://www.youtube.com/embed/video4",
+  ];
+
+  const topThreads = [
+    { id: 1, title: "Thread 1", author: "User1", comments: 10 },
+    { id: 2, title: "Thread 2", author: "User2", comments: 8 },
+    // Add more threads as needed
+  ];
 
   const sliderSettings = {
     dots: true,
@@ -33,119 +75,116 @@ const Home = () => {
     ],
   };
 
-  const recommendedCafes = [
-    {
-      id: 1,
-      name: 'TableTaft Boardgame Cafe',
-      link: '/TableTaft',
-      image: TableTaft,
-  },
-  {
-    id: 2,
-    name: 'Game Over PH: Respawn',
-    link: '/GameOver',
-    image: GameOver,
-},
-{
-  id: 3,
-  name: 'Hungry Meeples',
-  link: '/Hungry',
-  image: Hungry,
-},
-{
-  id: 4,
-  name: 'Happy Beans',
-  link: '/Happy',
-  image: Happy,
-},
-    
-  ];
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-  };
-
-  const topThreads = [
-    { id: 1, title: "Thread 1", author: "User1", comments: 10 },
-    { id: 2, title: "Thread 2", author: "User2", comments: 8 },
-    // Add more threads as needed
-  ];
-
   return (
     <>
-    <Hero/>
-    <div className="bg-gray min-h-screen">
-      <div className="container mx-auto -mt-[14%]">
-        <h2 className="text-2xl font-bold mb-4">
-          Discover the World of Board Games
-        </h2>
-        <p className="mb-8">
-          Explore a vast collection of board games for every taste and skill
-          level. From classic strategy games to modern party favorites, we have
-          it all. Whether you're a seasoned board gamer or just getting started,
-          our curated selection promises endless hours of fun.
-        </p>
-        <span className="mb-8 font-bold">
-          👫 Connect with Friends and Family
-        </span>
-        <p className="mt-8 mb-4">
-          Board games are best enjoyed with company! Invite your friends and
-          family to join you in exciting game nights. Strengthen bonds, create
-          lasting memories, and share laughter as you embark on thrilling board
-          game adventures together.
-        </p>
-
-          <Featured/>
-
-        <h2 className="text-2xl font-bold mb-4">
-          Schedule Ahead for Cafe Reservations
-        </h2>
-        <p className="mb-8">
-          Planning a special game night at one of our recommended cafes? Take
-          advantage of our reservation feature! Schedule ahead to secure your
-          spot and ensure an unforgettable gaming experience.
-        </p>
-
-        {/* Recommended Cafes */}
-        <div className="mb-8">
-          <h2 className="text-2xl font-bold mb-4">Recommended Cafes</h2>
-          <Slider {...sliderSettings}>
-            {recommendedCafes.map((cafe) => (
-              <div key={cafe.id}>
-                <Link to={cafe.link} className="flex flex-col items-center">
-                  <img
-                    src={cafe.image}
-                    alt={cafe.name}
-                    className="w-full h-40 object-cover mb-2 rounded"
-                  />
-                  <p className="text-gray-800 font-semibold">{cafe.name}</p>
-                </Link>
-              </div>
-            ))}
-          </Slider>
-        </div>
-
-        {/* Top Threads with Comments */}
-        <div>
-          <h2 className="text-2xl font-bold mb-4">Top Threads with Comments</h2>
-          <ul className="space-y-4">
-            {topThreads.map((thread) => (
-              <li key={thread.id}>
-                <Link to={`/forums/${thread.id}`}>
-                  <h3 className="text-lg font-semibold">{thread.title}</h3>
-                  <p className="text-gray-600">
-                    By {thread.author} - {thread.comments} Comments
-                  </p>
-                </Link>
-              </li>
-            ))}
-          </ul>
+      <Hero />
+      <div className="flex justify-center items-center relative -mt-2 bg-orange font-rubik">
+        <div className="text-white text-center font-rubik">
+          <h2 className="text-4xl font-bold mb-2 mt-2 text-black">PLAY, CONQUER, REPEAT</h2>
         </div>
       </div>
-    </div>
+        <div className="max-w-full lg:max-w-3/4 bg-white rounded-lg p-6 shadow-xl border">
+          <h2 className="text-3xl font-bold mb-4 text-black">
+            Discover the World of Board Games
+          </h2>
+          <p className="mb-6 text-xl text-black">
+            Explore a vast collection of board games for every taste and skill level. From classic strategy games to modern party favorites, we have it all. Whether you're a seasoned board gamer or just getting started, our curated selection promises endless hours of fun.
+          </p>
+          <p className="mb-6 text-3xl font-bold text-black">
+            👫 Connect with Friends and Family
+          </p>
+          <p className="mb-8 text-xl text-black">
+            So grab your favorite game, gather your friends, and let’s embark on a journey through the world of board games as we explore the lessons they teach us, the memories they create, and the stories they tell.
+          </p>
+        </div>
+        
+<div className="w-30 p-4">
+          <div className="mb-8 ">
+            <div className="relative">
+              <div className="w-[300px] h-[300px] ml-[20%] shadow-sm rounded overflow-hidden relative">
+                <img
+                  src={List1}
+                  alt="Sample"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="bg-gray bg-opacity-60 rounded-sm absolute mt-10 top-12 left-[40%] flex items-center justify-center text-black text-4xl font-bold">
+              The dice never lie; 
+              </div>
+              <div className="bg-gray bg-opacity-60 absolute mt-14 top-20 left-[40%] flex items-center justify-center text-black text-4xl font-bold">
+              they're a reflection of life's randomness.
+              </div>
+            </div>
+      
+        </div>
+            <Featured />
+            <div className="bg-gray text-black py-8">
+  <div className="container mx-auto text-center">
+    <h2 className="text-3xl font-bold mb-4">
+      Schedule Ahead for Cafe Reservations
+    </h2>
+    <p className="mb-6 text-lg">
+      Planning a special game night at one of our recommended cafes? Take advantage of our reservation feature! Schedule ahead to secure your spot and ensure an unforgettable gaming experience.
+    </p>
+  </div>
+</div>
+<div className="container mx-auto px-2 py-5 mb-5">
+  <div className="bg-black rounded-lg overflow-hidden shadow-md text-center p-4 mb-4 font-sora">
+    <h2 className="text-5xl font-bold font-montserrat text-white">Recommended Cafes</h2>
+  </div>
+  <Slider {...sliderSettings}>
+    {recommendedCafes.map((cafe) => (
+      <div key={cafe.id} className="px-1">
+        <Link to={cafe.link} className="block">
+          <div className="bg-white border rounded-lg overflow-hidden shadow-md">
+            <img
+              src={cafe.image}
+              alt={cafe.name}
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-2">
+              <h3 className="text-xl font-semibold mb-2 text-black">{cafe.name}</h3>
+            </div>
+          </div>
+        </Link>
+      </div>
+    ))}
+  </Slider>
+</div>
+            <div className="border">
+              <h2 className="text-3xl font-bold mb-4 bg-gray text-black py-8">Top Threads with Comments</h2>
+              <ul className="space-y-4">
+                {topThreads.map((thread) => (
+                  <li key={thread.id}>
+                    <Link to={`/forums/${thread.id}`}>
+                      <h3 className="text-xl font-semibold">{thread.title}</h3>
+                      <p className="text-gray-600">
+                        By {thread.author} - {thread.comments} Comments
+                      </p>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="container mx-auto py-4 lg:px-0 flex flex-col lg:flex-row gap-4">
+            <div className="w-full lg:w-1/4 p-4 bg-gray">
+              <h2 className="text-2xl font-bold mb-4">Game Tutorials</h2>
+              <div className="flex flex-wrap gap-2">
+                {videoLinks.map((link, index) => (
+                  <div key={index}>
+                    <iframe
+                      title={`Video ${index + 1}`}
+                      src={link}
+                      className="w-[200px] h-[200px] border rounded-md mb-2"
+                    />
+                  </div>
+                ))}
+              </div>
+              </div>
+              </div>
+              </div>
+
+
     </>
   );
 };
